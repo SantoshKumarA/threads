c26f4acdc6683adfd92fff18f38ae818e5aee4a9
Santosh Kumar A <santoshkumar.selva@gmail.com>
2016-08-26 20:28:00 +0530

    Closing resource..
    closing scanner resource..

--- Threads/src/demo109/reentrant/Application.java
+++ Threads/src/demo109/reentrant/Application.java
@@ -8,79 +8,83 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Application {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		final Processor processor = new Processor();
-		
-		Thread t1 = new Thread ( new Runnable() {
+
+		Thread t1 = new Thread(new Runnable() {
 			public void run() {
 				try {
-					processor.firstThread();	
-				} catch ( InterruptedException e) { }
+					processor.firstThread();
+				} catch (InterruptedException e) {
+				}
 			}
 		});
-		
-		Thread t2 = new Thread ( new Runnable () {
-			public void run () {
+
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
 				try {
 					processor.secondThread();
-				} catch (InterruptedException e) { }
+				} catch (InterruptedException e) {
+				}
 			}
 		});
-		
+
 		t1.start();
 		t2.start();
-		
+
 		t1.join();
 		t2.join();
-		
+
 		processor.finished();
 	}
 }
 
 class Processor {
-	
+
 	private int count = 0;
 	private Lock lock = new ReentrantLock();
 	private Condition condition = lock.newCondition();
-	
+
 	private void increment() {
-		for ( int i = 0; i < 10000; i ++)
+		for (int i = 0; i < 10000; i++)
 			count++;
 	}
-	
+
 	public void firstThread() throws InterruptedException {
 		lock.lock();
-		
+
 		System.out.println("Waiting..");
 		condition.await();
 		System.out.println("Woken up..");
-		
+
 		try {
 			increment();
 		} finally {
 			lock.unlock();
 		}
 	}
-	
+
 	public void secondThread() throws InterruptedException {
-		
+
 		Thread.sleep(1000);
-		
+
 		lock.lock();
-		
+
 		System.out.println("Press return key..");
-		new Scanner(System.in).nextLine();
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		scanner.close();
 		System.out.println("Got return key..");
-		
+
 		condition.signal();
-		
+
 		try {
-			increment();	
+			increment();
 		} finally {
 			lock.unlock();
 		}
 	}
-	
+
 	public void finished() {
 		System.out.println("Value of Count: " + count);
 	}
